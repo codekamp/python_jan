ebb6ab88092609a561af781b4012df80d3104c14
Prashant Chaudhary <101.prashant@gmail.com>
2018-03-17 13:28:26 +0530

    facebook API

--- codekamp/src/lostsoul_web/urls.py
+++ codekamp/src/lostsoul_web/urls.py
@@ -7,6 +7,7 @@ app_name = 'lostsoul_web'
 urlpatterns = [
     path('articles', views.list_articles, name='article_list'),
     path('articles/add', views.add_artile, name='add_new_article'),
+    path('oauth/facebook', views.facebook_oauth, name='facebook_oauth'),
     path('articles/<str:my_slug>', views.get_article, name='article_detail'),
     path('auth/login', auth_views.LoginView.as_view(), name='login_page')
 ]
--- codekamp/src/lostsoul_web/views.py
+++ codekamp/src/lostsoul_web/views.py
@@ -1,28 +1,37 @@
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 from pip._vendor import requests
 
 from lostsoul_web.models import Article
 
 
 def list_articles(request):
+    state = get_random_string(32)
+    user_city = 'Ghaziabad'
+
+    print(request.user.id)
+    cache.set(state, request.user.id, 6000)
+
     # ip_addr = request.META.get('REMOTE_ADDR')
-    ip_addr = '43.225.0.133'
-
-    res = requests.get('http://freegeoip.net/json/' + ip_addr).json()
-    user_city = res['city']
-
-    credentials = {'password': 'rajneesh10', 'email': '101.prashant@gmail.com'}
-    res = requests.get('https://api.invidz.com/api/authenticate', params=credentials).json()
-
-    token = res['token']
-    print(res['user']['first_name'])
-
-
-    token_header = {'Authorization': 'bearer ' + token}
-    videos = requests.get('https://api.invidz.com/api/videos', headers=token_header).json()
-
-    print(videos['data'][3]['title'])
+    # ip_addr = '43.225.0.133'
+    #
+    # res = requests.get('http://freegeoip.net/json/' + ip_addr).json()
+    # user_city = res['city']
+    #
+    # credentials = {'password': 'rajneesh10', 'email': '101.prashant@gmail.com'}
+    # res = requests.get('https://api.invidz.com/api/authenticate', params=credentials).json()
+    #
+    # token = res['token']
+    # print(res['user']['first_name'])
+    #
+    #
+    # token_header = {'Authorization': 'bearer ' + token}
+    # videos = requests.get('https://api.invidz.com/api/videos', headers=token_header).json()
+    #
+    # print(videos['data'][3]['title'])
     query = ''
     try:
         query = request.GET['q']
@@ -30,16 +39,19 @@ def list_articles(request):
     except:
         articles = Article.objects.all()
 
-    return render(request, 'list-articles.html', {'my_articles': articles, 'query': query, 'city': user_city})
+    return render(request, 'list-articles.html',
+                  {'my_articles': articles, 'query': query, 'city': user_city, 'state': state})
+
 
 def get_article(request, my_slug):
     article = Article.objects.get(slug=my_slug)
     return render(request, 'get-article.html', {'article': article})
 
+
 @login_required()
 def add_artile(request):
-    title=''
-    content=''
+    title = ''
+    content = ''
     error = False
 
     if request.method == 'POST':
@@ -53,5 +65,24 @@ def add_artile(request):
             article = Article.objects.create(title=title, content=content, author=author)
             return redirect('lostsoul_web:article_detail', article.slug)
 
-
     return render(request, 'add-article.html', {'title': title, 'content': content, 'error': error})
+
+
+def facebook_oauth(request):
+    grant = request.GET['code']
+    state = request.GET['state']
+
+    user_id = cache.get(state)
+
+    if (user_id == request.user.id):
+        client_secret = ''
+        data = {'client_id': '', 'redirect_uri': 'http://localhost:8080/oauth/facebook',
+                'client_secret': client_secret, 'code': grant}
+        res = requests.get(
+            'https://graph.facebook.com/v2.12/oauth/access_token', params=data).json()
+        request.user.facebook_token = res['access_token']
+        request.user.save()
+
+        print(res['access_token'])
+
+        return redirect('lostsoul_web:add_new_article')
